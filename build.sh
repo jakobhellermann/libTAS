84b08e5171e5b9165e83260aa7988d7e038b3981
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-05-18 15:33:15 +0200

    Add QT linuxdeploy plugin to build.sh

--- build.sh
+++ build.sh
@@ -15,9 +15,10 @@ make -j4
 # Build .AppImage
 
 wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/1-alpha-20250213-1/linuxdeploy-plugin-qt-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
 APPDIR_PATH=$(readlink -f ./AppDir)
 
-make prefix=/usr DESTDIR=$APPDIR_PATH install
-ARCH=x86_64 ./linuxdeploy*.AppImage --appimage-extract-and-run --appdir AppDir/ --output appimage
+make prefix=/usr "DESTDIR=$APPDIR_PATH" install
+ARCH=x86_64 ./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir AppDir/ --plugin qt --output appimage

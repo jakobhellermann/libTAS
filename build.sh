ef159a4b8db40ef4d10e30193545a7cb7ce8cb51
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-08-08 21:50:06 +0200

    build: fix appimage building
    - NO_STRIP due to https://github.com/linuxdeploy/linuxdeploy/issues/272
    - qt plugin seems to be necessary
    - update to cmake

--- build.sh
+++ build.sh
@@ -8,10 +8,12 @@ cmake --build build -j"$(nproc)"
 
 # Build .AppImage
 
-# wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-# chmod +x linuxdeploy*.AppImage
+wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -nc https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/1-alpha-20250213-1/linuxdeploy-plugin-qt-x86_64.AppImage
 
-# APPDIR_PATH=$(readlink -f ./AppDir)
+chmod +x linuxdeploy*.AppImage
 
-# make prefix=/usr DESTDIR=$APPDIR_PATH install
-# ARCH=x86_64 ./linuxdeploy*.AppImage --appimage-extract-and-run --appdir AppDir/ --output appimage
+APPDIR_PATH=$(readlink -f ./AppDir)
+DESTDIR="$APPDIR_PATH" cmake --install build --config Release --prefix /usr
+
+NO_STRIP=true ARCH=x86_64 ./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir AppDir/ --plugin qt --output appimage
